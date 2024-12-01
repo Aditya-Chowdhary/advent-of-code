82b0e36210cffa080a17862978f14540f0e198cb
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-12-02 02:38:35 +0530

    Day 1, Part 1

--- 2024/elixir/lib/advent_of_code/solution/year_2024/day_01.ex
+++ 2024/elixir/lib/advent_of_code/solution/year_2024/day_01.ex
@@ -1,5 +1,19 @@
 defmodule AdventOfCode.Solution.Year2024.Day01 do
-  def part1(_input) do
+  def part1(input) do
+    test =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.split/1)
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.map(&Enum.sort/1)
+      |> Enum.zip()
+      |> Enum.reduce(0, fn item, accumulator ->
+        accumulator + abs(String.to_integer(elem(item, 0)) - String.to_integer(elem(item, 1)))
+      end)
+
+    IO.inspect(test, limit: :infinity)
+    test
   end
 
   def part2(_input) do
