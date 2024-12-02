0d82d01830cd5ae67eece007d2763d4cf314367a
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-12-02 05:50:02 +0530

    Day 1, Part 1 improvements

--- 2024/elixir/lib/advent_of_code/solution/year_2024/day_01.ex
+++ 2024/elixir/lib/advent_of_code/solution/year_2024/day_01.ex
@@ -1,19 +1,21 @@
 defmodule AdventOfCode.Solution.Year2024.Day01 do
   def part1(input) do
-    test =
-      input
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.split/1)
-      |> Enum.zip()
-      |> Enum.map(&Tuple.to_list/1)
-      |> Enum.map(&Enum.sort/1)
-      |> Enum.zip()
-      |> Enum.reduce(0, fn item, accumulator ->
-        accumulator + abs(String.to_integer(elem(item, 0)) - String.to_integer(elem(item, 1)))
-      end)
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      String.split(line)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.sort/1)
+    |> Enum.zip()
+    |> Enum.reduce(0, fn {a, b}, acc ->
+      acc + abs(a - b)
+    end)
 
-    IO.inspect(test, limit: :infinity)
-    test
+    # IO.inspect(test, limit: :infinity)
+    # test
   end
 
   def part2(_input) do
