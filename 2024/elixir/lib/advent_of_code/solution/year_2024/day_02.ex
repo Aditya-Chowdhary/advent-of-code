816836d3723d18a9bae61f3042edab95a24042c7
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-12-04 02:47:33 +0530

    Day 2, Part 1

--- 2024/elixir/lib/advent_of_code/solution/year_2024/day_02.ex
+++ 2024/elixir/lib/advent_of_code/solution/year_2024/day_02.ex
@@ -1,5 +1,59 @@
 defmodule AdventOfCode.Solution.Year2024.Day02 do
-  def part1(_input) do
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      String.split(line)
+      |> Enum.map(&String.to_integer/1)
+
+      # |> IO.inspect(charlists: :as_list)
+    end)
+    |> Enum.count(fn line ->
+      safe?(:start, 0, 0, line)
+    end)
+  end
+
+  def ssafe(:start, _, _, list) do
+    safe?(:start, 0, 0, list)
+  end
+
+  defp safe?(0, a, b, []) when abs(a - b) in 1..3 do
+    a < b
+  end
+
+  defp safe?(1, a, b, []) when abs(a - b) in 1..3 do
+    a > b
+  end
+
+  defp safe?(:start, _, _, list) do
+    [a | t] = list
+    [b | newt] = t
+    [c | finalt] = newt
+
+    cond do
+      abs(a - b) not in 1..3 ->
+        false
+
+      a < b ->
+        safe?(0, b, c, finalt)
+
+      a > b ->
+        safe?(1, b, c, finalt)
+    end
+  end
+
+  defp safe?(0, a, b, list) when abs(a - b) in 1..3 do
+    [c | t] = list
+    if(a < b, do: safe?(0, b, c, t), else: false)
+  end
+
+  defp safe?(1, a, b, list) when abs(a - b) in 1..3 do
+    [c | t] = list
+    if(a > b, do: safe?(1, b, c, t), else: false)
+  end
+
+  defp safe?(_, _, _, _) do
+    false
   end
 
   def part2(_input) do
