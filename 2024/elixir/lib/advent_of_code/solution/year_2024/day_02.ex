7356d36091842e67e2cb760a464a968ddcc70477
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-12-04 08:46:25 +0530

    Day 2, Part 2

--- 2024/elixir/lib/advent_of_code/solution/year_2024/day_02.ex
+++ 2024/elixir/lib/advent_of_code/solution/year_2024/day_02.ex
@@ -9,53 +9,56 @@ defmodule AdventOfCode.Solution.Year2024.Day02 do
       # |> IO.inspect(charlists: :as_list)
     end)
     |> Enum.count(fn line ->
-      safe?(:start, 0, 0, line)
+      safe?(:start, line)
     end)
   end
 
-  def ssafe(:start, _, _, list) do
-    safe?(:start, 0, 0, list)
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      String.split(line)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.count(fn line ->
+      damp =
+        0..length(line)
+        |> Enum.map(&List.delete_at(line, &1))
+
+      safe?(:start, line) || Enum.any?(damp, &safe?(:start, &1))
+    end)
   end
 
-  defp safe?(0, a, b, []) when abs(a - b) in 1..3 do
-    a < b
-  end
+  def ssafe(:start, list), do: safe?(:start, list)
 
-  defp safe?(1, a, b, []) when abs(a - b) in 1..3 do
-    a > b
-  end
+  defp safe?(:start, [a, b | tail]) when (b - a) in 1..3, do: safe?(:inc, [b | tail])
+  defp safe?(:start, [a, b | tail]) when (a - b) in 1..3, do: safe?(:dec, [b | tail])
+  defp safe?(:inc, [a, b | tail]) when (b - a) in 1..3, do: safe?(:inc, [b | tail])
+  defp safe?(:dec, [a, b | tail]) when (a - b) in 1..3, do: safe?(:dec, [b | tail])
+  defp safe?(_, [_]), do: true
+  defp safe?(_, _), do: false
 
-  defp safe?(:start, _, _, list) do
-    [a | t] = list
-    [b | newt] = t
-    [c | finalt] = newt
+  # ! Part 2 Functions
 
-    cond do
-      abs(a - b) not in 1..3 ->
-        false
+  # defp safe?(_incdec, _, n) when n > 1, do: false
+  # Start setting flag
+  # 5 7 3 1
+  # 5 7 6 7
+  # 5 3 8 10
+  # 5 3 4 3
+  # defp safe?(:start, [a, b, c | tail]) when abs(b-a) in 1..3 and abs(c-b) in 1..3 do
+  #   x = b-a
+  #   y = c-b
+  # end
 
-      a < b ->
-        safe?(0, b, c, finalt)
-
-      a > b ->
-        safe?(1, b, c, finalt)
-    end
-  end
-
-  defp safe?(0, a, b, list) when abs(a - b) in 1..3 do
-    [c | t] = list
-    if(a < b, do: safe?(0, b, c, t), else: false)
-  end
-
-  defp safe?(1, a, b, list) when abs(a - b) in 1..3 do
-    [c | t] = list
-    if(a > b, do: safe?(1, b, c, t), else: false)
-  end
-
-  defp safe?(_, _, _, _) do
-    false
-  end
-
-  def part2(_input) do
-  end
+  # 5 7 2 8
+  # 5 3 8 1
+  # defp safe?(:start, [a, b | tail], n) when (b - a) in 1..3, do: safe?(:inc, [b | tail], n)
+  # defp safe?(:start, [a, b | tail], n) when (a - b) in 1..3, do: safe?(:dec, [b | tail], n)
+  # # Cases where immediate next succeeds.
+  # defp safe?(:inc, [a, b | tail], n) when (b - a) in 1..3, do: safe?(:inc, [b | tail], n)
+  # defp safe?(:dec, [a, b | tail], n) when (a - b) in 1..3, do: safe?(:dec, [b | tail], n)
+  # # Cases where we ignore b
+  # defp safe?(:inc, [a, _b, c | tail], n) when (c - a) in 1..3, do: safe?(:inc, [c | tail], n + 1)
+  # defp safe?(:dec, [a, _b, c | tail], n) when (a - c) in 1..3, do: safe?(:dec, [c | tail], n + 1)
 end
